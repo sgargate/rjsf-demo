98e29bc85d85e111c43bcf92300acabe1866221c
Siddharth <siddharth@finzy.com>
2023-01-27 12:30:11 +0530

    mui form

--- src/components/dynamicForm/DynamicForm.tsx
+++ src/components/dynamicForm/DynamicForm.tsx
@@ -1,3 +1,23 @@
+import { RJSFSchema } from "@rjsf/utils";
+import Form from "@rjsf/mui";
+import validator from "@rjsf/validator-ajv8";
+
+
+
+const schema: RJSFSchema = {
+    title: "Todo",
+    type: "object",
+    required: ["title"],
+    properties: {
+      title: {type: "string", title: "Title", default: "A new task"},
+      done: {type: "boolean", title: "Done?", default: false}
+    }
+  };
+  const log = (type: any) => console.log.bind(console, type);
 export default function DynamicForm() {
-    return <>Hello world</>
+    return <Form schema={schema}
+    validator={validator}
+    onChange={log("changed")}
+    onSubmit={log("submitted")}
+    onError={log("errors")} />
 }
