b119d6d2a40baf5602936dc3bb2eaeda632fa6fb
Siddharth <siddharth@finzy.com>
2023-01-27 13:14:14 +0530

    example form

--- src/components/dynamicForm/DynamicForm.tsx
+++ src/components/dynamicForm/DynamicForm.tsx
@@ -1,22 +1,26 @@
-import { RJSFSchema } from "@rjsf/utils";
+import { RJSFSchema , UiSchema} from "@rjsf/utils";
 import Form from "@rjsf/mui";
 import validator from "@rjsf/validator-ajv8";
+import FirstStepSchema from "../first-step-schema";
 
 
 
-const schema: RJSFSchema = {
-    title: "Todo",
-    type: "object",
-    required: ["title"],
-    properties: {
-      title: {type: "string", title: "Title", default: "A new task"},
-      done: {type: "boolean", title: "Done?", default: false}
-    }
+const schema: RJSFSchema = FirstStepSchema;
+
+  const uiSchema: UiSchema = {
+   /*  "title": {
+      "ui:classNames": "custom-title"
+    } */
   };
+
+  const formData = {
+    loanDetails: {tenure: "6 months"}
+  };
+
   const log = (type: any) => console.log.bind(console, type);
 export default function DynamicForm() {
     return <Form schema={schema}
-    validator={validator}
+    validator={validator} uiSchema={uiSchema}  formData = {formData}
     onChange={log("changed")}
     onSubmit={log("submitted")}
     onError={log("errors")} />
---
+++ src/components/first-step-schema.ts
@@ -0,0 +1,99 @@
+import { RJSFSchema } from "@rjsf/utils";
+
+var FirstStepSchema: RJSFSchema = {
+  title: "Basic Details",
+  type: "object",
+  //required: ["tenure"],
+  properties: {
+    loanDetails: {
+      type: "object",
+      title: "Loan Details",
+      required: ["tenure"],
+      properties: {
+        group: {
+          type: "string",
+          "anyOf": [
+            {
+              "type": "string",
+              "enum": [
+                "fortis"
+              ],
+              "title": "Fortis Hospital"
+            },
+            {
+              "type": "string",
+              "enum": [
+                "sahyadriPune"
+              ],
+              "title": "Sahyadri Pune"
+            },
+            {
+              "type": "string",
+              "enum": [
+                "apollo"
+              ],
+              "title": "Apollo Mumbai"
+            }
+          ],
+          title: "Hospital Name"
+        },
+        amount: {
+          type: "integer",
+          minimum: 5000,
+          maximum: 1000000,
+          title: "Loan amount"
+        },
+        tenure: {
+          type: "string",
+          title: "Loan Tenure",
+          "anyOf": [
+            {
+              "type": "string",
+              "enum": [
+                "6"
+              ],
+              "title": "Six Months"
+            },
+            {
+              "type": "string",
+              "enum": [
+                "12"
+              ],
+              "title": "12 Months"
+            },
+            {
+              "type": "string",
+              "enum": [
+                "24"
+              ],
+              "title": "24 Months"
+            }
+          ]
+        }
+      }
+    },
+    applicantDetails: {
+      type: "object",
+      title: "Applicant Details",
+      properties: {
+        name: {
+          title: "Applicant name",
+          type: "string"
+        },
+        dob: {
+          type: "string",
+          format: "date",
+          title: "Date of Birth"
+        },
+        "email": {
+          type: "string",
+          format: "email",
+          title: "Email"
+
+        }
+      }
+    }
+  }
+};
+
+export default FirstStepSchema;
