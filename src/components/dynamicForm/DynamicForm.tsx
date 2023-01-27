8cf9f5795b23c3f068bd25fd9e22c16d88052148
Siddharth <siddharth@finzy.com>
2023-01-27 14:20:40 +0530

    flex layout for grouped fields

--- src/components/dynamicForm/DynamicForm.tsx
+++ src/components/dynamicForm/DynamicForm.tsx
@@ -1,17 +1,63 @@
-import { RJSFSchema , UiSchema} from "@rjsf/utils";
+import { RJSFSchema , UiSchema, ObjectFieldTemplateProps} from "@rjsf/utils";
 import Form from "@rjsf/mui";
 import validator from "@rjsf/validator-ajv8";
 import FirstStepSchema from "../first-step-schema";
+import Box, {BoxProps} from "@mui/material/Box";
 
+
+function Item(props: BoxProps) {
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        p: 1,
+        m: 1,
+        width: '100%',
+        maxWidth: '200px',
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+}
 
 
 const schema: RJSFSchema = FirstStepSchema;
 
   const uiSchema: UiSchema = {
+    loanDetails: {
+      "ui:ObjectFieldTemplate": ObjectFieldTemplate
+
+    }
    /*  "title": {
       "ui:classNames": "custom-title"
     } */
   };
+
+  function ObjectFieldTemplate(props: ObjectFieldTemplateProps) {
+    return (
+      <div>
+        {props.title}
+        {props.description}
+        <Box sx={{ 
+          display: 'flex',
+    //      flexDirection: 'row',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
+          p: 1,
+          m: 1,
+          borderRadius: 1, }}>
+        {props.properties.map(element => 
+          <Item>
+            {element.content}
+            </Item>
+        )
+        
+        }
+        </Box>
+      </div>
+    );
+  }
 
   const formData = {
     loanDetails: {tenure: "6 months"}
@@ -20,7 +66,7 @@ const schema: RJSFSchema = FirstStepSchema;
   const log = (type: any) => console.log.bind(console, type);
 export default function DynamicForm() {
     return <Form schema={schema}
-    validator={validator} uiSchema={uiSchema}  formData = {formData}
+    validator={validator} uiSchema={uiSchema}  formData = {formData} 
     onChange={log("changed")}
     onSubmit={log("submitted")}
     onError={log("errors")} />
